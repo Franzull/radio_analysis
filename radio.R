3ece7e5c14ef949af711698d14c7579d120a921b
Franziska Ullmann <105322318+Franzull@users.noreply.github.com>
2022-05-11 21:58:24 +0200

    Tidy version
    tidied up some unnecessary lines.

--- radio.R
+++ radio.R
@@ -5,26 +5,25 @@ library(jsonlite)
 res = GET("http://all.api.radio-browser.info/json/stations")
 data = fromJSON(rawToChar(res$content))
 
-country_occurenceCount = table(data$countrycode) # shows amount of stations for each countrycode
+country_occurenceCount = table(data$countrycode)
 
 # Relative Frequencies
 
 # remove first element since it is shows the stations without country code
 country_occurenceCount_withoutFirstValue = country_occurenceCount[-1]
 
-list_occurenceCount = list(country_occurenceCount_withoutFirstValue)
-# count all stations that have a countrycode
+# count all stations that have a country code
 allStations = sum(country_occurenceCount_withoutFirstValue)
 
-relativeFrequency = lapply(list_occurenceCount, function(x) x/allStations)
-relativeFrequency_list = lapply(relativeFrequency, function(x) x*100)
+relativeFrequency_list = lapply(country_occurenceCount_withoutFirstValue, function(x) (x/allStations)*100)
 
 # only display TOP10 Countries, the rest in other
 sortedRelativeFrequency = sort(unlist(relativeFrequency_list), decreasing = TRUE)
 top10AndOther = sortedRelativeFrequency[1:10]
 top10AndOther = c(top10AndOther, setNames(sum(sortedRelativeFrequency[11:length(sortedRelativeFrequency)]),"other"))
 
+# pie chart
 lbls = paste(names(top10AndOther), ":", round(top10AndOther), "%")
-pie(top10AndOther, main = "Ratio of TOP 10 ", labels = lbls)
+pie(top10AndOther, main = "Percentages of TOP10 Countries with Most Radio Stations", labels = lbls)
 
-    
+
